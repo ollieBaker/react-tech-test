aadcae4491f4ad08dedc5e232d1dbe5ea7c9dfcd
ollieBaker <oliverjohnbaker@gmail.com>
2024-02-08 19:53:59 +0000

    fix: reset scroll position when loading beer details

--- src/views/BeerDetail.tsx
+++ src/views/BeerDetail.tsx
@@ -2,9 +2,14 @@ import { useLoaderData } from 'react-router-dom';
 import { BeerObject } from '../types/types';
 
 import BeerDetailList from '../components/BeerDetail/BeerDetail';
+import { useEffect } from 'react';
 
 export default function BeerDetail() {
   let beer = useLoaderData() as BeerObject;
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <section className="beer-detail-page">
