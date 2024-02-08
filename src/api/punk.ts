0e81c3049fb535c72850a4bb32f3bd5d51fd3d24
ollieBaker <oliverjohnbaker@gmail.com>
2024-02-08 17:10:02 +0000

    feat: implement infinite scroll button and add simple api caching

--- src/api/punk.ts
+++ src/api/punk.ts
@@ -1,12 +1,21 @@
 import { BeerObject, IPage } from '../types/types';
+//Simple implementation of API response caching
+const apiCache = {};
 
 const fetchWithErrorHandling = async (url) => {
+  if (apiCache.hasOwnProperty(url)) {
+    console.log('returning data from cache');
+    return apiCache[url];
+  }
+
   const response = await fetch(url);
   if (response.status !== 200) {
     throw new Error('An Api error occurred');
   }
 
-  return await response.json();
+  const data = await response.json();
+  apiCache[url] = data;
+  return data;
 };
 
 export async function getBeersByPage({
@@ -14,9 +23,10 @@ export async function getBeersByPage({
   pageSize,
 }: IPage): Promise<Array<BeerObject>> {
   try {
-    const data = await fetchWithErrorHandling(
-      `https://api.punkapi.com/v2/beers?page=${page}&per_page=${pageSize || 10}`
-    );
+    const url = `https://api.punkapi.com/v2/beers?page=${page}&per_page=${
+      pageSize || 10
+    }`;
+    const data = await fetchWithErrorHandling(url);
 
     return data;
   } catch (error) {
@@ -30,9 +40,8 @@ export async function getBeerById({
   id: number | string;
 }): Promise<BeerObject> {
   try {
-    const data = await fetchWithErrorHandling(
-      `https://api.punkapi.com/v2/beers/${id}`
-    );
+    const url = `https://api.punkapi.com/v2/beers/${id}`;
+    const data = await fetchWithErrorHandling(url);
 
     return data[0];
   } catch {
--- src/views/Beers.tsx
+++ src/views/Beers.tsx
@@ -1,14 +1,50 @@
 import { BeerObject } from '../types/types';
-import DrinkList from '../components/DrinkList.js';
+import DrinkList from '../components/DrinkList/DrinkList.js';
 import { useLoaderData } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getBeersByPage } from '../api/punk';
 
 export default function Drinks() {
-  let beers = useLoaderData() as BeerObject[];
+  const initialBeers = useLoaderData() as BeerObject[];
+
+  const [page, setPage] = useState(1);
+  const [beers, setBeers] = useState(initialBeers);
+  const [isLoading, setisLoading] = useState(false);
+  const [beerRemaining, setBeerRemaining] = useState(true);
+
+  const loadMoreBeer = async () => {
+    setPage((p) => p + 1);
+  };
+
+  useEffect(() => {
+    const getBeer = async () => {
+      if (page > 1) {
+        setisLoading(true);
+        const data = await getBeersByPage({ page });
+        if (!data.length || data.length < 10) setBeerRemaining(false);
+        setBeers([...beers, ...data]);
+        setisLoading(false);
+      }
+    };
+    getBeer();
+
+    return () => {};
+  }, [page]);
 
   return (
     <main>
       <h1>Beer App</h1>
       <DrinkList drinkData={beers} path="beer" />
+      {beerRemaining ? (
+        <p>
+          Thirsty?{' '}
+          <button disabled={isLoading} onClick={loadMoreBeer}>
+            Get more beer here!
+          </button>
+        </p>
+      ) : (
+        <p>You drunk the bar dry!</p>
+      )}
     </main>
   );
 }
